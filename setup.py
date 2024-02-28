87cfd87b2e2086e1a57ba04c30ff9d50700e1eec
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2024-02-28 13:51:29 +0100

    Upgrade Lambda runtime to Python 3.11

--- setup.py
+++ setup.py
@@ -14,12 +14,15 @@ setup(
     url="https://github.com/oslokommune/okdata-log-group-subscriber",
     packages=find_packages(),
     install_requires=[
-        "aws-xray-sdk",
-        "boto3",
+        "aws-xray-sdk>=2.12,<3",
+        "boto3>=1.28.11",
         # XXX: Don't upgrade this package to 7.14 or later, since it doesn't
         # work with AWS' OpenSearch Service.
         "elasticsearch<7.14",
-        "okdata-aws>=1.0.1,<2.0.0",
+        "okdata-aws>=2,<3",
         "requests-aws4auth",
+        # Not used directly, it's a transitive dependency from `aws-xray-sdk`,
+        # but we need version 1.14 or above to make it work with Python 3.11.
+        "wrapt>=1.14,<2",
     ],
 )
